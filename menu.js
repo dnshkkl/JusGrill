302cac360e94ff24cb843deb0bc9fff9bb611a11
danishkurukkoli <155357857+danishkurukkoli@users.noreply.github.com>
2024-05-10 21:51:01 +0530

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,10 @@
+<script>
+    function toggleMenu() {
+        var menuSection = document.getElementById("menu-section");
+        if (menuSection.style.display === "none") {
+            menuSection.style.display = "block";
+        } else {
+            menuSection.style.display = "none";
+        }
+    }
+</script>
