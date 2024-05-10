949616b1094c4ae8d39c7bdbceac28b587118902
danishkurukkoli <155357857+danishkurukkoli@users.noreply.github.com>
2024-05-10 22:07:38 +0530

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,10 +1,8 @@
-<script>
-    function toggleMenu() {
-        var menuSection = document.getElementById("menu-section");
-        if (menuSection.style.display === "none") {
-            menuSection.style.display = "block";
-        } else {
-            menuSection.style.display = "none";
-        }
+function toggleMenu() {
+    var menuSection = document.getElementById("menu-section");
+    if (menuSection.style.display === "none") {
+        menuSection.style.display = "block";
+    } else {
+        menuSection.style.display = "none";
     }
-</script>
+}
